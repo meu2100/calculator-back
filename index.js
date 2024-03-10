5985d52bc9233a479da3cbec4caf155357af6fc8
j32u4ukh <j32u4ukh@gmail.com>
2024-03-10 12:17:46 +0800

    feat: 實作加減乘除四個 API

--- index.js
+++ index.js
@@ -7,12 +7,67 @@ const app = express()
 // 設置監聽的 port (後端 URL 會是 localhost:[PORT])
 const PORT = 3000
 
+// 小數點後保留位數
+const ROUND = 5
+
 // 處理 GET localhost:[PORT] 的請求(註：localhost:[PORT] 和 localhost:[PORT]/ 是等價的，結尾有無 / 都一樣)
 // req: 前端發送來的請求物件
 // res: 前端發送來的請求物件
 app.get('/', (req, res) => {
   // 以字串形式返回
   res.send('This is backend of Calculator.')
+})
+
+// 處理 GET localhost:[PORT]/plus?v1=xxx&v2=xxx 的請求
+// ex:
+// request: /plus?v1=3&v2=7
+// response: { answer: 10 }
+app.get('/plus', (req, res) => {
+  const QUERY = req.query
+  // v1 和 v2 必須為數字，目前尚未處理非數字的情況
+  const v1 = Number(QUERY.v1)
+  const v2 = Number(QUERY.v2)
+  res.json({
+    answer: v1 + v2,
+  })
+})
+
+// ex:
+// request: /minus?v1=3&v2=7
+// response: { answer: -4 }
+app.get('/minus', (req, res) => {
+  const QUERY = req.query
+  const v1 = Number(QUERY.v1)
+  const v2 = Number(QUERY.v2)
+  res.json({
+    answer: v1 - v2,
+  })
+})
+
+// ex:
+// request: /multiply?v1=3&v2=7
+// response: { answer: 21 }
+app.get('/multiply', (req, res) => {
+  const QUERY = req.query
+  const v1 = Number(QUERY.v1)
+  const v2 = Number(QUERY.v2)
+  res.json({
+    answer: v1 * v2,
+  })
+})
+
+// ex:
+// request: /divide?v1=3&v2=7
+// response: { answer: 0.42857 }
+app.get('/divide', (req, res) => {
+  const QUERY = req.query
+  const v1 = Number(QUERY.v1)
+  const v2 = Number(QUERY.v2)
+  // 預設只保留小數點後 ROUND 位
+  const answer = Number((v1 / v2).toFixed(ROUND))
+  res.json({
+    answer,
+  })
 })
 
 app.listen(PORT, () => {
