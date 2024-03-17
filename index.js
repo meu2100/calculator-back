f00e979873a4079a0f8021245e4df9b49d72c805
j32u4ukh <j32u4ukh@gmail.com>
2024-03-17 22:46:46 +0800

    feat: 導入 CORS，允許跨域請求

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // 引用後端框架 express
 const express = require('express')
+const cors = require('cors')
 
 // 建立 express 實例
 const app = express()
+
+// 使用 cors 中間件，允許不同網域來的請求，免於同源策略的限制
+app.use(cors())
 
 // 設置監聽的 port (後端 URL 會是 localhost:[PORT])
 const PORT = 3000
