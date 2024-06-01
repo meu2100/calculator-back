93dc015db0c40048feb34c2f0724b803dd57cc38
meu2100 <amanda8018@gmail.com>
2024-06-01 19:46:11 +0800

    Setting route named as service

---
+++ Service/index.js
@@ -0,0 +1,6 @@
+//建立基本路由
+const calculate = require('./calculator')
+const history = require('./history')
+
+
+module.exports = { calculate, history}
--- route/calculate.js
+++ route/calculate.js
@@ -7,40 +7,6 @@ route.get('/', (req, res)=>{
 })
 
 
-route.get('/add', (req, res) => {
-  const query = req.query
-  const v1 = Number(query.v1)
-  const v2 = Number(query.v2)
-  res.json({
-    answer: v1 + v2,
-  })
-})
+//加減乘除運算
 
-route.get('/minus', (req, res) => {
-  const query = req.query
-  const v1 = Number(query.v1)
-  const v2 = Number(query.v2)
-  res.json({
-    answer: v1 - v2,
-  })
-})
-
-route.get('/multiply', (req, res) => {
-  const query = req.query
-  const v1 = Number(query.v1)
-  const v2 = Number(query.v2)
-  res.json({
-    answer: v1 * v2,
-  })
-})
-
-route.get('/divide', (req, res) => {
-  const query = req.query
-  const v1 = Number(query.v1)
-  const v2 = Number(query.v2)
-  // 預設只保留小數點後 ROUND 位
-  const answer = Number((v1 / v2).toFixed(ROUND))
-  res.json({
-    answer,
-  })
-})
+module.exports = route
