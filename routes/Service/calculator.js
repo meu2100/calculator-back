9159dd1d3a4b927c3af6ead252712b1e18149231
meu2100 <amanda8018@gmail.com>
2024-06-02 15:12:20 +0800

    adding calculator function

--- routes/Service/calculator.js
+++ routes/Service/calculator.js
@@ -1,5 +1,27 @@
-//加減乘除運算
-
 //導入歷史紀錄
 const history = require('./history')
+
+//列出加減乘除運算式
+
+function add(V1, V2){
+  return V1 + V2
+}
+
+function minus(V1, V2){
+  return V1 - V2
+}
+
+function multiply(V1, V2){
+  return V1 * V2
+}
+
+function divide(V1, V2){
+  //設定除法小數點
+  const round = 5
+  if(V2 === 0){
+    console.log('分母不得為0')
+  } else{
+    return(V1 / V2).toFixed(round)
+  }
+}
 
