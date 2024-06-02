b85f50422d6a9a96bfcfcb280c76a4632bf29d21
meu2100 <amanda8018@gmail.com>
2024-06-02 14:17:04 +0800

    setting history record function

--- routes/Service/calculator.js
+++ routes/Service/calculator.js
@@ -1,1 +1,5 @@
 //加減乘除運算
+
+//導入歷史紀錄
+const history = require('./history')
+
--- routes/Service/history.js
+++ routes/Service/history.js
@@ -1,1 +1,23 @@
 //記錄每筆計算資料
+
+
+class HistoryRecord {
+  constructor(){
+    this.HistoryRecord = []
+  }
+
+add(formula, answer) {
+  this.HistoryRecord.push({
+    formula: formula,
+    answer: answer
+  })
+ }
+
+getHistory(){
+  return this.HistoryRecord
+ }
+}
+
+const history = new HistoryRecord
+
+module.exports = history
